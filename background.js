fffa80d4966cb193817cb3d92d0303c15a29c149
Alisa Modeste <user@alisamodeste.com>
2013-02-22 07:13:45 -0500

    Added copyright notice.

--- background.js
+++ background.js
@@ -1,3 +1,23 @@
+/*
+
+	Copyright 2013 Alisa Modeste
+
+	This file is part of BookmarkNests.
+
+    BookmarkNests is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 3 as published by
+    the Free Software Foundation.
+
+    BookmarkNests is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with BookmarkNests.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 chrome.browserAction.onClicked.addListener(function(tab) {
 	
 //This is to put the manager one tab to the right
